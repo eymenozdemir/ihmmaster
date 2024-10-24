02446f64505ceb7e182e773a67e43e57917342c9
eymenozdemir <51022719+eymenozdemir@users.noreply.github.com>
2024-10-24 06:30:27 +0300

    Update baseUrl.js

--- admin/src/utils/baseUrl.js
+++ admin/src/utils/baseUrl.js
@@ -1,2 +1,3 @@
 //export const base_url = "https://api.stonenaturesystem.com/api/";
-export const base_url = "http://localhost:4000/api/";
+//export const base_url = "http://localhost:4000/api/";
+export const base_url = "https://ihmmaster-backend.onrender.com/api/";
