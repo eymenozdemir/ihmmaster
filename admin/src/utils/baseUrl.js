ca8bfb588d3d2862a7006be805073b075f6e49c8
Eymen Özdemir <eymenozdemir@Eymens-MacBook-Pro.local>
2024-11-01 02:35:49 +0300

    vps deploy

--- admin/src/utils/baseUrl.js
+++ admin/src/utils/baseUrl.js
@@ -1,3 +1,4 @@
 //export const base_url = "https://api.stonenaturesystem.com/api/";
 //export const base_url = "http://localhost:4000/api/";
-export const base_url = "https://ihmmaster-backend.onrender.com/api/";
+//export const base_url = "https://ihmmaster-backend.onrender.com/api/";
+export const base_url = "https://api.ihmexpert.net/api/";
